5aa57c06598b1c82460c49f3a3e07ca54d4209d5
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-09-27 22:30:13 +0000

    create skeleton

--- src/main.zig
+++ src/main.zig
@@ -10,117 +10,35 @@ pub const SignalId = u64;
 pub fn BijectMap(comptime K: type, comptime V: type) type {
     return struct {
         a: std.mem.Allocator,
-        k2v: std.AutoHashMap(K, std.AutoHashMap(V, void)),
-        v2k: std.AutoHashMap(V, std.AutoHashMap(K, void)),
+        // todo: add fields here
 
-        pub const Iterator = std.AutoHashMap(K, void).KeyIterator;
-
-        pub fn init(a: std.mem.Allocator) @This() {
-            return .{
-                .a = a,
-                .k2v = FieldType(@This(), .k2v).init(a),
-                .v2k = FieldType(@This(), .v2k).init(a),
-            };
-        }
-        pub fn deinit(this: @This()) void {
-            var _this = this;
-            {
-                var it = _this.k2v.valueIterator();
-                while (it.next()) |arr| arr.deinit();
-                _this.k2v.deinit();
-            }
-            {
-                var it = _this.v2k.valueIterator();
-                while (it.next()) |arr| arr.deinit();
-                _this.v2k.deinit();
-            }
-        }
-
+        pub const Iterator = undefined;
         pub const ClearOptions = struct {
             retain_memory: bool = true,
             assert_empty: bool = false,
         };
+
+        pub fn init(a: std.mem.Allocator) @This() {
+            return .{ .a = a };
+        }
+        pub fn deinit(this: @This()) void {
+            _ = this;
+        }
         pub fn clearValues(this: *@This(), key: K, opts: ClearOptions) void {
-            const entry = this.k2v.getEntry(key) orelse return;
-            const values_map = entry.value_ptr;
-            defer {
-                if (opts.retain_memory) {
-                    values_map.clearRetainingCapacity();
-                } else {
-                    this.k2v.removeByPtr(entry.key_ptr);
-                    values_map.deinit();
-                }
-            }
-
-            if (opts.assert_empty)
-                assert(values_map.count() == 0);
-
-            var it = values_map.keyIterator();
-            while (it.next()) |value| {
-                const entry_set = this.v2k.getEntry(value.*) orelse unreachable;
-                const set = entry_set.value_ptr;
-                defer {
-                    if (set.count() == 0) {
-                        if (opts.retain_memory) {
-                            set.clearRetainingCapacity();
-                        } else {
-                            set.deinit();
-                            this.v2k.removeByPtr(entry_set.key_ptr);
-                        }
-                    }
-                }
-                const remove_ok = set.remove(key);
-                if (!remove_ok) unreachable;
-            }
+            _ = opts;
+            _ = key;
+            _ = this;
         }
         pub fn add(this: *@This(), key: K, value: V) !void {
-            {
-                const entry = try this.k2v.getOrPut(key);
-                if (!entry.found_existing) {
-                    entry.value_ptr.* = @TypeOf(entry.value_ptr.*).init(this.a);
-                }
-                try entry.value_ptr.put(value, void{});
-            }
-            {
-                const entry = try this.v2k.getOrPut(value);
-                if (!entry.found_existing) {
-                    entry.value_ptr.* = @TypeOf(entry.value_ptr.*).init(this.a);
-                }
-                try entry.value_ptr.put(key, void{});
-            }
+            _ = value;
+            _ = key;
+            _ = this;
         }
         /// iterate keys that has value
         pub fn iteratorByValue(this: *const @This(), value: V) ?Iterator {
-            const keys = this.v2k.get(value) orelse return null;
-            return keys.keyIterator();
-        }
-        pub fn dumpLog(this: @This()) void {
-            {
-                std.log.warn("k2v:", .{});
-                var it = this.k2v.iterator();
-                while (it.next()) |entry| {
-                    const key = entry.key_ptr.*;
-
-                    var it1 = entry.value_ptr.*.iterator();
-                    while (it1.next()) |entry1| {
-                        const value = entry1.key_ptr.*;
-                        std.log.warn("{} -> {}", .{ key, value });
-                    }
-                }
-            }
-            {
-                std.log.warn("v2k:", .{});
-                var it = this.v2k.iterator();
-                while (it.next()) |entry| {
-                    const key = entry.key_ptr.*;
-
-                    var it1 = entry.value_ptr.*.iterator();
-                    while (it1.next()) |entry1| {
-                        const value = entry1.key_ptr.*;
-                        std.log.warn("{} -> {}", .{ key, value });
-                    }
-                }
-            }
+            _ = value;
+            _ = this;
+            return undefined;
         }
     };
 }
